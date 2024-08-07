e47122e8043aa1a457aea42191409c8e3d6e5059
IanAriel <iabaquero-duque@myseneca.ca>
2024-08-06 23:11:45 -0400

    Update preprocess-iot-monitor-customdata.py

--- iotsolution-scripts-data/preprocess-iot-monitor-customdata.py
+++ iotsolution-scripts-data/preprocess-iot-monitor-customdata.py
@@ -169,8 +169,10 @@ latlong=lat:long'
      streamstojoin="" 
      identifier = "IoT device performance and failures"
 
+     preprocesslogic='MIN,MAX,COUNT,VARIANCE,OUTLIERS,ANOMPROB'
+
      # if dataage - use:dataage_utcoffset_timetype
-     preprocesslogic='anomprob,trend,avg'
+     #preprocesslogic='anomprob,trend,avg'
 
      pathtotmlattrs='oem=n/a,lat=n/a,long=n/a,location=n/a,identifier=n/a'          
      try:
